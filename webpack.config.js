c690cdb316607934c28df91de90aaa48bd7a0ed8
lucas-burdell <lucasburdell@gmail.com>
2019-09-19 21:16:21 -0500

    add CNAME to build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -13,7 +14,10 @@ module.exports = {
       favicon: path.resolve(__dirname, "./src/assets/img/favicon.png"),
       inject: "body",
       hash: true,
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: path.resolve(__dirname, 'CNAME') }
+    ])
   ],
   output: {
     path: path.resolve(__dirname, "docs"),
