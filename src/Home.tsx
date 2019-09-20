45e099581d33750cfcf133adf7c0f369ae8d7fb4
lucas-burdell <lucasburdell@gmail.com>
2019-09-19 21:03:24 -0500

    fix date format for momentjs

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,7 +6,7 @@ import { Paragraph } from "./Paragraph";
 import { FancyLink } from "./FancyLink";
 import { StaticLogo } from "./StaticLogo";
 
-const hiredDate = "August 10, 2018 08:00:00 CST";
+const hiredDate = "10 Aug 2018 08:00:00 CST";
 
 export const Home: FunctionComponent = () => (
   <Fragment>
@@ -17,20 +17,33 @@ export const Home: FunctionComponent = () => (
       <Paragraph>
         Hey, I'm Lucas Burdell! Writer, gamer, tech enthusiast, software
         developer and 3D printer hobbyist. Science and technology are my main
-        passions, and passion is the fuel that drives me to tinker and build. My day
-        job is as a software developer, which gives me an outlet for my
+        passions, and passion is the fuel that drives me to tinker and build. My
+        day job is as a software developer, which gives me an outlet for my
         creativity and also keeps the lights on!
       </Paragraph>
       <Paragraph>
-        I've been a software developer for <b><CountupClock date={hiredDate} /></b>.
+        I've been a software developer for{" "}
+        <b>
+          <CountupClock date={hiredDate} />
+        </b>
+        .
         <br />
-        However, I've always been interested in computer programming. I wrote my first program somewhere in 2009.
+        However, I've always been interested in computer programming. I wrote my
+        first program somewhere in 2009.
       </Paragraph>
       <Paragraph>
         I believe in collaboration: if you have a cool project that you're
         working on that you'd think I'd want to help with, feel free to ping me
-        at <FancyLink href={"mailto:lucas@lucasburdell.com?Subject=Interested+in+My+Cool+Project%3F"}>lucas@lucasburdell.com</FancyLink>.
-        Please don't spam me. I promise I'll read your emails, and will respond if I feel like it. 😉
+        at{" "}
+        <FancyLink
+          href={
+            "mailto:lucas@lucasburdell.com?Subject=Interested+in+My+Cool+Project%3F"
+          }
+        >
+          lucas@lucasburdell.com
+        </FancyLink>
+        . Please don't spam me. I promise I'll read your emails, and will
+        respond if I feel like it. 😉
       </Paragraph>
     </Content>
   </Fragment>
